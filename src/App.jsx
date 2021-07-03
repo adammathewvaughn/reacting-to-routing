7d562a413cb92e97eef249927e9252bea604c850
Adam Vaughn <adammathewvaughn>
2021-07-03 00:33:41 -0500

    a decent start

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ import React from "react";
 const App = () => {
     return (
       <>
-        <h1>Hello, world!</h1>
+      <div className="h-100 w-100 ">
+      <div className="d-flex justify-content-center">
+      <nav className="navbar navbar-expand-lg h-550px navbar-light bg-dark d-flex justify-content-center ">
+<ul className="nav mt-3 mb-3">
+<li className="nav-item">
+<a className="nav-link " href="https://code.visualstudio.com/"  target="_blank" rel="noopener noreferrer">Home</a>
+</li>
+<li className="nav-item mx-5">
+<a className="nav-link" href="https://ghibliapi.herokuapp.com/films/"  target="_blank" rel="noopener noreferrer">Link Films</a>
+</li>
+<li className="nav-item">
+<a className="nav-link " href="https://code.visualstudio.com/"  target="_blank" rel="noopener noreferrer">Link People</a>
+</li>
+</ul>
+</nav>
+      </div>  
+      </div>
       </>
     );
   };
